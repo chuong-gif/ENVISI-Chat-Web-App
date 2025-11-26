caf3edb4426cf4775ee740160c4a8fa343e4d9b1
Ngo Van Chuong <chuong17102005@gmail.com>
2025-11-26 09:45:01 +0700

    api signin (đăng nhập) đã được tạo và hoạt động

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,11 @@
 import bcrypt from 'bcrypt';
 import User from "../models/User.js";
+import Session from "../models/Session.js";
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+
+const ACCESS_TOKEN_TTL = '30m'; // thường thì access token chỉ có dưới 15 phút thôi
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000; // refresh token trong 14 ngày
 
 export const signUp = async (req, res) => {
     try {
@@ -23,7 +29,7 @@ export const signUp = async (req, res) => {
             username,
             hashedPassword,
             email,
-            displayName: '${firstName} ${lastName}'
+            displayName: `${firstName} ${lastName}`
         });
 
         // return
@@ -33,5 +39,62 @@ export const signUp = async (req, res) => {
         console.error("Lỗi khi gọi signUp", error);
         return res.status(500).json({ message: "Lỗi hệ thống" });
 
+    }
+};
+
+export const signIn = async (req, res) => {
+    try {
+        // lấy inputs
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ message: "thiếu username hoặc password" });
+        }
+        // lấy hashedPassword trong db để so sánh với password input
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ message: "username hoặc password không chính xác" });
+        }
+
+        // kiểm tra password
+
+        const passwordCorrect = await bcrypt.compare(password, user.hashedPassword);
+
+        if (!passwordCorrect) {
+            return res.status(401).json({ message: "username hoặc password không chính xác" });
+        }
+
+        // nếu khớp tọa access token với JWT
+        const accessToken = jwt.sign(
+            { userId: user._id },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: ACCESS_TOKEN_TTL }
+        );
+        // tạo refresh token
+
+        const refreshToken = crypto.randomBytes(64).toString("hex");
+
+
+        // tạo session mới để lưu refresh token
+        await Session.create({
+            userId: user._id,
+            refreshToken,
+            expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+        });
+
+        // trả refresh token về trong cookie
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true, // cookie này không thể truy cập bằng javascipt
+            secure: true, // đảm bảo chỉ gửi qua https
+            sameSite: 'none', // cho backend và frontend chạy trên 2 doman khác nhau (deploy riêng)
+            maxAge: REFRESH_TOKEN_TTL
+        })
+
+        // trả access token về trong res
+        return res.status(200).json({ message: `User ${user.displayName} đã log in!`, accessToken })
+
+    } catch (error) {
+        console.error("lỗi gọi signIn", error);
+        return res.status(500).json({ message: "Lỗi hệ thống" });
     }
 };
